25ae5c70840c718b0f21368bac587e10de281059
Abdul Sami <sami.kdr@live.se>
2022-05-18 17:45:29 +0200

    break

---
+++ backend/src/Robot.spec.ts
@@ -0,0 +1,23 @@
+import { Robot, IPosition } from './Robot'
+
+/* 
+    Writing the initial room grids, and starting position of the robot
+*/
+
+const grid = {
+  x: 5,
+  y: 5,
+}
+
+const position: IPosition = {
+  x: 3,
+  y: 3,
+  direction: 'N',
+}
+
+/* 
+
+    I demonstrate to go for the following unit tests, but there can be many more.
+    According to my knowledge of the logic, these tests should be enough to approve the robot logic successfull.
+
+*/
