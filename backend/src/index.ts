14be21f721738f5c0200f4b98e66d6ebadc65905
Abdul Sami <sami.kdr@live.se>
2022-05-18 21:00:31 +0200

    body-parser dep added to support client post req to the server for the robot creation, fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express'
 
+import { json } from 'body-parser'
+
+import { Robot } from './Robot'
+
 const app = express()
 
+app.use(json())
+
 app.post('/robot', (req: Request, res: Response) => {
-  res.json({ message: 'server reponse' })
+  const { position, grid } = req.body
+
+  const robot = new Robot({ grid, position })
+
+  res.json(robot.toString())
 })
 
 app.listen(3000, () => console.log('Server listening on port 3000...'))
