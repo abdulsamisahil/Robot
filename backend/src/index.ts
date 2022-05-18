9b4981646aa800b0c97f6a439e7387be93cc29da
Abdul Sami <sami.kdr@live.se>
2022-05-18 23:29:33 +0200

    express validator dep to check the post body position and grid object and types and so on, test ok!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,38 @@ import { json } from 'body-parser'
 
 import { Robot } from './Robot'
 
+import { body, validationResult } from 'express-validator'
+
 const app = express()
 
 app.use(json())
 
-app.post('/robot', (req: Request, res: Response) => {
-  const { position, grid } = req.body
+app.post(
+  '/robot',
+  isPositionValid('position'),
+  body('position.direction').isIn(['N', 'S', 'E', 'W']),
+  isPositionValid('grid'),
+  (req: Request, res: Response) => {
+    const errors = validationResult(req)
 
-  const robot = new Robot({ grid, position })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
 
-  res.json(robot.toString())
-})
+    const { position, grid } = req.body
+
+    const robot = new Robot({ grid, position })
+
+    res.json(robot.toString())
+  }
+)
+
+function isPositionValid(field: string) {
+  return [
+    body(field).isObject(),
+    body(`${field}.x`).isNumeric().toInt(),
+    body(`${field}.y`).isNumeric().toInt(),
+  ]
+}
 
 app.listen(3000, () => console.log('Server listening on port 3000...'))
